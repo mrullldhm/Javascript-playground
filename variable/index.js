9e6da66797e30b061a077c4e025754dc188cd14b
mrullldhm <mrullldhm94@gmail.com>
2025-10-02 13:25:45 +0800

    Variable Javascript

---
+++ variable/index.js
@@ -0,0 +1,14 @@
+let fullName = `Amirul Adham`
+let age = 31
+let student = false
+
+document.getElementById("p1").textContent = `My name is ${fullName}`
+document.getElementById("p2").textContent = `My age is ${age}`
+document.getElementById("p3").textContent = `The status of student is ${student}`
+
+console.log(fullName)
+console.log(typeof fullName)
+console.log(age)
+console.log(typeof age)
+console.log(student)
+console.log(typeof student)
