fce89aaf7dd4f78d015543b2535aa5b9def0544e
mrullldhm <mrullldhm94@gmail.com>
2025-10-02 22:11:44 +0800

    Type Conversion Javascript

---
+++ type-conversion/index.js
@@ -0,0 +1,15 @@
+let x = 31;
+let y = 31;
+let z = "";
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
