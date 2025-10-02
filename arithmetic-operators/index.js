dc5ff563f13b33228536170b2d0216702cd90887
mrullldhm <mrullldhm94@gmail.com>
2025-10-02 14:35:35 +0800

    Arithmetic Operators Javascript

---
+++ arithmetic-operators/index.js
@@ -0,0 +1,32 @@
+let student = 30;
+
+let student1 = student + 1;
+console.log(student1);
+let student2 = student - 1;
+console.log(student2);
+let student3 = student * 1;
+console.log(student3);
+let student4 = student / 1;
+console.log(student4);
+let student5 = student ** 2;
+console.log(student5);
+let student6 = student % 4;
+console.log(student6);
+
+student += 1;
+console.log(student);
+student -= 1;
+console.log(student);
+student *= 2;
+console.log(student);
+student /= 2;
+console.log(student);
+student **= 2;
+console.log(student);
+student %= 7;
+console.log(student);
+
+student++;
+console.log(student);
+student --
+console.log(student);
