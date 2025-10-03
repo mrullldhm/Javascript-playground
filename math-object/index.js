3f4961f404330de94272d403e49f53f265c3d388
mrullldhm <mrullldhm94@gmail.com>
2025-10-04 01:52:52 +0800

    Math Object Javascript

---
+++ math-object/index.js
@@ -0,0 +1,48 @@
+let x = 3.78123;
+let y = 9;
+let z = 1;
+
+z = Math.PI;
+console.log(z);
+
+z = Math.round(x);
+console.log(z);
+
+z = Math.floor(x);
+console.log(z);
+
+z = Math.ceil(x);
+console.log(z);
+
+z = Math.trunc(x);
+console.log(z);
+
+z = Math.pow(y, x);
+console.log(z);
+
+z = Math.sqrt(y);
+console.log(z);
+
+z = Math.log(x);
+console.log(z);
+
+z = Math.sin(x);
+console.log(z);
+
+z = Math.cos(x);
+console.log(z);
+
+z = Math.tan(x);
+console.log(z);
+
+z = Math.abs(x);
+console.log(z);
+
+z = Math.sign(x);
+console.log(z);
+
+let Min = Math.min(x, y, z);
+console.log(Min);
+
+let Max = Math.max(x, y, z);
+console.log(Max);
