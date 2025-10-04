7eba31a70b3f46bba6b069d925659843f15873cf
mrullldhm <mrullldhm94@gmail.com>
2025-10-05 01:59:59 +0800

    Ternary Operator Javascript

---
+++ ternary-operator/index.js
@@ -0,0 +1,15 @@
+let age = 18;
+let message = age >= 18 ? "You are an adult" : "You are underage";
+console.log(message);
+
+time = 12;
+let greeting = time >= 12 ? "Good afternoon dude" : "Good morning badass";
+console.log(greeting);
+
+let isStudent = true;
+let status = isStudent ? "You are a student" : "You are not a student";
+console.log(status);
+
+let purchase = 10;
+let discount = purchase >= 100 ? 0.1 : 0;
+console.log(`Your total is ${purchase - purchase * discount}`);
