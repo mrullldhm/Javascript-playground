89f57f5335b11e394a30366c3af0ea04df098fbc
mrullldhm <mrullldhm94@gmail.com>
2025-10-07 02:37:38 +0800

    Logical Operators Javascript

---
+++ logical-operators/index.js
@@ -0,0 +1,15 @@
+const temp = 20;
+
+if (temp >= 0 && temp < 30) {
+  console.log("Its a good weather today");
+} else {
+  console.log("Its a bad weather today");
+}
+
+const isSunny = false;
+
+if (!isSunny) {
+  console.log("It is Sunny");
+} else {
+  console.log("It is not Sunny");
+}
