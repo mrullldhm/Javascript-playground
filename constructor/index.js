0c06177def3c1221c5f2ce845da2d322eb3070e1
mrullldhm <mrullldhm94@gmail.com>
2025-10-14 02:59:05 +0800

    Constructor Javascript

---
+++ constructor/index.js
@@ -0,0 +1,20 @@
+// Constructor = Special method for defining the properties and methods of object
+
+function Car(make, model, year, color) {
+  (this.make = make),
+    (this.model = model),
+    (this.year = year),
+    (this.color = color);
+  this.drive = function () {
+    console.log(`Your car model is ${this.model}`);
+  };
+}
+
+const car1 = new Car("Ford", "Mustang", "2025", "Black");
+
+console.log(car1);
+console.log(car1.make);
+console.log(car1.model);
+console.log(car1.year);
+console.log(car1.color);
+car1.drive();
