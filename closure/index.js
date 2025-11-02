8bc102d745ef2f66c674b95d5ac5cf5ca1f77958
Muhamad Amirul Adham <154552382+mrullldhm@users.noreply.github.com>
2025-11-02 08:48:05 +0800

    Closure Javascript

--- closure/index.js
+++ closure/index.js
@@ -22,11 +22,45 @@ function createCounter() {
     count++;
     console.log(`Count increased to ${count}`);
   }
+
+  function getCount() {
+    return count;
+  }
+
   // return {increment:increment}
-  return { increment };
+  return { increment, getCount };
 }
 
 const counter = createCounter();
 counter.increment();
 counter.increment();
 counter.increment();
+console.log(`The current count is ${counter.getCount()}`);
+
+// Example 3
+function createGame() {
+  let score = 0;
+
+  function increaseScore(points) {
+    score += points;
+    console.log(`+${points}points`);
+  }
+
+  function decreaseScore(points) {
+    score -= points;
+    console.log(`+${points}points`);
+  }
+
+  function getScore() {
+    console.log(`The final score is ${score} points`);
+    return score;
+  }
+
+  return { increaseScore, decreaseScore, getScore };
+}
+
+let game = createGame();
+
+game.increaseScore(9);
+game.decreaseScore(1);
+game.getScore();
